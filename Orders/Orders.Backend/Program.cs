6c7acf0aabc4b70ce177e8ef726416d11049c942
Miguel Angel Estrada Lopez <766miki@gmail.com>
2025-05-22 23:29:48 -0500

    conecciones a doker

--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -1,15 +1,22 @@
+using Orders.Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+// Swagger (documentación de la API)
 builder.Services.AddSwaggerGen();
+
+// Registrar DataContext con la cadena de conexión
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DockerConnection"))
+);
+
+// Otros servicios necesarios (como controladores)
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware para Swagger
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +24,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
